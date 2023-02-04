21ba58c80d3cb1f55208e00c7bfaf2625c7fbcca
Adam Lopez <adamlopez446@gmail.com>
2023-02-04 14:22:14 -0500

    inital commit of paragraph parsing logic + unit tests

---
+++ paragraph_parser.py
@@ -0,0 +1,59 @@
+from typing import Dict, List, Set
+from collections import defaultdict
+import re
+import string
+
+class ParagraphParser:
+
+    alphanumeric_characters = set(string.ascii_letters + string.digits + ' ')
+
+    def __init__(self,paragraph):
+        self.paragraph = paragraph
+        self.tokenized_paragraph = self._get_all_valid_words(paragraph)
+        self.all_characters = self._get_all_valid_characters(paragraph)
+
+
+    def _get_all_valid_words(self, paragraph: str) -> List[str]:
+        words = paragraph.split()
+        valid_words = []
+        for word in words:
+            chars = [c for c in word]
+            if all([c in self.alphanumeric_characters for c in chars]):
+                valid_words.append(word)
+        return valid_words
+
+
+    def _get_all_valid_characters(self,paragraph: str) -> List[str]:
+        return [char_ for char_ in paragraph if char_ in self.alphanumeric_characters]
+
+
+    def get_word_frequency(self) -> defaultdict[str,int]:
+        """count the repetition of each word (alphanumeric). 
+        the characters array should contain only English words and numbers 
+        and they should be alphabetically ordered 
+        (numbers first followed by words)."""
+        word_freq = defaultdict(lambda :0)
+        for token in self.tokenized_paragraph:
+            word_freq[token] += 1
+            print(word_freq)
+        return dict(sorted(word_freq.items()))
+
+
+    def get_word_count(self) -> int:
+        """get the total number of valid words in the paragraph."""
+        return len(self.tokenized_paragraph)
+    
+
+    def get_character_count(
+        self, 
+        exclude: Set[str]=set()
+    ) -> int:
+        """"return the total number of characters. 
+        if exclude is not empty, the total number of occurences of characters 
+        not in the set will be returned.
+        if both include and exclude are empty, the total number of characters will be returned.
+        """
+        filtered_characters = [char for char in self.all_characters if char not in exclude]
+        return len(filtered_characters)
+
+        
---
+++ tests/test_paragraph_parser.py
@@ -0,0 +1,61 @@
+import pytest
+from paragraph_parser import ParagraphParser
+import re
+
+
+def test_characters_all_valid():
+    paragraph = "one 1 day also "
+    parser = ParagraphParser(paragraph)
+    assert  parser.tokenized_paragraph == ['one','1','day','also']
+
+
+def test_exclude_foreign_characters():
+    paragraph = "word 是 word2"
+    parser = ParagraphParser(paragraph)
+    assert  parser.tokenized_paragraph == ['word','word2']
+    
+
+def test_exclude_emojis():
+    paragraph = "abcdefg123 👀aa 🙂"
+    parser = ParagraphParser(paragraph)
+    assert  parser.tokenized_paragraph == ['abcdefg123']
+
+def test_exclude_multiple_spaces():
+    paragraph = "abcdefg123    another word"
+    parser = ParagraphParser(paragraph)
+    assert  parser.tokenized_paragraph == ['abcdefg123','another','word']
+
+def test_word_count():
+    paragraph =  'this is a test this this this'
+    parser = ParagraphParser(paragraph)
+    word_freq = parser.get_word_frequency()
+    assert word_freq == {'a': 1,'is': 1,'test':1,'this': 4}
+
+
+def test_word_count_with_numbers():
+    paragraph =  'this is a test 4 this 4 this this 123'
+    parser = ParagraphParser(paragraph)
+    word_freq = parser.get_word_frequency()
+    assert word_freq == {'123': 1, '4':2, 'a': 1,'is': 1,'test':1,'this': 4}
+
+def test_init_character_array():
+    paragraph =  ' this is a test 4 '
+    parser = ParagraphParser(paragraph)
+    assert parser.all_characters == \
+        [' ', 't', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 't', 'e', 's', 't', ' ', '4', ' ']
+
+def test_get_character_count():
+    paragraph =  ' this is a test 4 '
+    parser = ParagraphParser(paragraph)
+    assert parser.get_character_count() == 18
+
+def test_get_character_count_no_spaces():
+    paragraph =  ' this is a test 4 '
+    parser = ParagraphParser(paragraph)
+    assert parser.get_character_count(exclude={' '}) == 12
+
+def  test_get_word_count():
+    paragraph =  'this is a test 4 '
+    parser = ParagraphParser(paragraph)
+    assert parser.get_word_count() == 5
+
